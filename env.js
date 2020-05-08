6cc3607cfbcc53ebd602bcdd51b5599448632f38
Dimas Agusta Wiranata <dimaswiranata8@gmail.com>
2020-05-09 02:07:12 +0700

    13. Showing a Map Preview of the Location

---
+++ env.js
@@ -0,0 +1,5 @@
+const vars = {
+  googleApiKey: 'AIzaSyBe6Txbe3bkskxk4jyEVG2Qge8QfNCcLkc'
+};
+
+export default vars;
--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -5,6 +5,7 @@ import { useDispatch } from 'react-redux';
 import Colors from '../constants/Colors';
 import ImagePicker from '../components/ImagePicker';
 import * as placesActions from '../store/places/actions';
+import LocationPicker from '../components/LocationPicker';
 
 const NewPlaceScreen = props => {
   const [titleValue, setTitleValue] = useState('');
@@ -36,6 +37,7 @@ const NewPlaceScreen = props => {
           value={titleValue}
         />
         <ImagePicker onImageTaken={imageTakenHandler}/>
+        <LocationPicker/>
         <Button 
           title='Save Place' 
           color={Colors.primary}
